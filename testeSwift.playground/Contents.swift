5bcf8370ac8c7b90b1584bb4db3010a3aab8a886
rethink-daniel <daniel.vidal@rethink.dev>
2022-02-17 10:47:30 -0300

    Primeiro commit com os4 apps realizados até agora

---
+++ testeSwift.playground/Contents.swift
@@ -0,0 +1,139 @@
+import UIKit
+
+/*
+var list = Set<String>()
+
+list.insert("Leite")
+list.insert("Pao")
+list.insert("mamao")
+
+print(list)
+
+
+print("---------------------For com array dicionario(keys e value)-----------------------")
+
+var meses = [Int: String]()
+
+meses[0] = "Janeiro"
+meses[1] = "Fevereiro"
+meses[2] = "Março"
+
+print(meses[1]!)
+
+for mes in meses{
+    print(mes)
+}
+
+print("---------------------For com vetor-----------------------")
+
+
+var pubs: [String] = []
+
+pubs.append("20 % DE DESCONTO APROVEITE")
+pubs.append("Compre hoje e ganhe um cupom para amanha")
+pubs.append("cansado hj? Compre em nosso site com frete gratis")
+
+for pub in pubs{
+    print(pub)
+}
+
+var cont = 0
+
+while cont < 10 {
+    cont += 1
+    print(cont)
+}
+
+print(cont)
+
+repeat{
+    print(cont)
+    cont += 1
+} while cont < 10*/
+
+
+//print("---------------------Funçao-----------------------")
+//
+//func multiple(num1: Int,num2: Int){
+//    print("Essa é a funcáo que multiplica")
+//    let result = num1 * num2
+//    print(result)
+//}
+//
+//
+//var numero1: Int = 10
+//var numero2: Int = 2
+//multiple(num1: numero1, num2: numero2)
+
+//print("---------------------Funçao que return-----------------------")
+//
+//func multiple(num1: Int,num2: Int) -> Int{
+//    print("Essa é a funcáo que multiplica e retorna")
+//    let result = num1 * num2
+//    return result
+//}
+//
+//
+//var numero1: Int = 10
+//var numero2: Int = 2
+//var soma = 15 + multiple(num1: numero1, num2: numero2)
+//print(soma)
+
+
+//print("---------------------Uso de Class(gets and sets)-----------------------")
+//
+//class Home{
+//
+//    var color: String
+//    //Contructor
+//    init(color: String){
+//        self.color = color
+//    }
+//
+//    func getColor() -> String{
+//        return self.color
+//    }
+//    func setColor(newColor: String){
+//        color = newColor
+//    }
+//}
+//
+//var home = Home(color: "Yellow")
+//
+//home.setColor(newColor: "Blue")
+//print(home.getColor())
+
+//print("---------------------Uso de Class(Heranças)-----------------------")
+//
+//class Animal{
+//    var color: String
+//    //Contructor
+//    init(color: String){
+//        self.color = color
+//    }
+//    func dormir() -> String{
+//        return"dorme"
+//    }
+//
+//}
+//class Dog: Animal{
+//    func latir() -> String{
+//        return "sim ele late"
+//    }
+//}
+//class Bird: Animal{
+//    func voar() -> String{
+//        return "sim ele vooa"
+//    }
+//}
+//
+//print("---------New Dog---------")
+//var dog1 = Dog(color: "Red")
+//print(dog1.dormir())
+//print(dog1.latir())
+//
+//print("--------New Bird---------")
+//var bird1 = Bird(color: "Blue")
+//print(bird1.dormir())
+//print(bird1.voar())
+
