b73e9fe69b7e888f4cec580cf13b8e86c159a52c
rethink-daniel <daniel.vidal@rethink.dev>
2022-03-02 22:46:00 -0300

    App Anotações e desenvolvimento do salvamento de dados

---
+++ Anotacoes/Anotacoes/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Anotacoes
+//
+//  Created by Rethink on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextView!
+    let key = "userNote"
+    
+    @IBAction func buttonSave(_ sender: Any) {
+        if let text = textField.text{
+            UserDefaults.standard.set(text, forKey: key)
+        }
+        
+    }
+    
+    func recuperaNote() -> String{
+        if let textRecover = UserDefaults.standard.object(forKey: key){
+            return textRecover as! String
+        }
+        return ""
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        textField.text = recuperaNote()
+        
+    }
+
+  
+     
+}
+
---
+++ Anotaçoes/SaveData/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Anotaçoes
+//
+//  Created by Rethink on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UserDefaults. é usado para salvar pequenas infos
+        //UserDefaults.standard.set("azul", forKey: "corFundo") //--> esse metodo add
+        //UserDefaults.standard.removeObject(forKey: "corFundo") // --> esse remove
+        //let text = UserDefaults.standard.object(forKey: "corFundo")
+        //let foods: [String] = ["Lasanha","Sucrilhos", "Pizza"]
+        //UserDefaults.standard.set(foods , forKey: "foods")
+        //
+        //print(text)
+    }
+
+
+}
+
